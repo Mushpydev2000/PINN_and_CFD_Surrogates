620d4ceca24427a211ad8a322e20b214203deeab
Mohammed_Bahauddin <moh.mush21@gmail.com>
2026-06-10 11:30:26 +0530

    Use Vercel Python handler for redirect

--- api/redirect.py
+++ api/redirect.py
@@ -1,15 +1,17 @@
 import os
 
-from vercel import Response
+from http.server import BaseHTTPRequestHandler
 
-def handler(request):
-    render_url = os.environ.get('RENDER_APP_URL', 'https://pinn-and-cfd-surrogates.onrender.com')
-    return Response(
-        status=302,
-        headers={
-            'Location': render_url,
-        },
-        body=''
-    )
 
-app = handler
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        render_url = os.environ.get(
+            "RENDER_APP_URL",
+            "https://pinn-and-cfd-surrogates.onrender.com",
+        )
+        self.send_response(302)
+        self.send_header("Location", render_url)
+        self.end_headers()
+
+    def do_HEAD(self):
+        self.do_GET()
